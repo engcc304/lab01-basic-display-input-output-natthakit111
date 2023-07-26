a687a290d0e582408090e41d35175809904d9900
natthakit111 <natthakit_ya66@live.ac.th>
2023-07-26 16:54:49 +0700

    updet lab4

--- Lab/Lab04-Variable-Operator.cpp
+++ Lab/Lab04-Variable-Operator.cpp
@@ -28,20 +28,24 @@
 #include <stdio.h>
 
 int main() {
-    int id ;
-    int hrs ;
-    float Salary ;
-    
-    printf( "Enter your Employee ID =" ) ;
-    scanf( "%d", &id ) ;
-    printf( "Enter your working hour =" ) ;
-    scanf( "%d", &hrs ) ;
-    printf( "Enter your houry =" ) ;
-    scanf( "%f", &Salary ) ;
-    
-    printf( "Expected = \n" ) ;
-    printf( "Employee ID = %d \n", id ) ;
-    printf( "Salary = %0.2f", hrs * Salary ) ;
+
+    //--| YOUR CODE HERE
+    char EmployeeID[10] ;
+    float hours ;
+    float salaryperhours ;
+    float salary ; 
+
+    printf( "Input the Employees ID: \n" ) ;
+    scanf( "%s", EmployeeID);
+    printf( "Input the working hrs: \n" ) ;
+    scanf( "%f", &hours ) ;
+    printf( "Salary amount/hr: \n" ) ;
+    scanf( "%f", &salaryperhours ) ;
+
+    salary = salaryperhours * hours ;
+    printf( "Expected Output: \n" ) ;
+    printf( "Employees ID = %s \n",  EmployeeID ) ;
+    printf( "Salary = U$ %0.2f \n", salary ) ;
 
     return 0 ;
 }//end main function
